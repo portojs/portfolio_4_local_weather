9e5aefcfc345ccd12ed85c8909b0147e384fd42b
peter <peter.k.nets@gmail.com>
2016-05-05 11:23:17 +0300

    Test icons

--- src/index.js
+++ src/index.js
@@ -9,19 +9,28 @@ class Main extends React.Component {
 
     window.onload = function() {
 
+      var tempUnit = '\u00B0C';
+
       function success(pos) {
         var link = 'http://api.openweathermap.org/data/2.5/weather?lat=' +
                 pos.coords.latitude + '&lon=' + pos.coords.longitude +
-                '&units=metric&APPID=7b5fd7c59b65645c55cc078c587e19bb';
+                '&units=metric&APPID=7b5fd7c59b65645c55cc078c587e19bb',
+            icons = ['01d', '02d', '03d', '04d', '09d', '10d', '11d', '13d', '50d'],
+            iconClasses = ['wi-owm-day-800', 'wi-owm-804', ''];
+
         $.getJSON(link, function(data) {
           console.log(data);
+          console.log(data.weather[0].id);
+          console.log(data.weather[0].icon);
+
           document.getElementById('local-weather').innerHTML = data.weather[0].description;
-          document.getElementById('temperature').innerHTML = data.main.temp;
+          document.getElementById('temperature').innerHTML = Math.floor(data.main.temp);
           document.getElementById('location').innerHTML = data.name;
 
           document.getElementById('input-field').value = data.name;
-          document.getElementById('weather-card-temperature').innerHTML = data.main.temp;
-          document.getElementById('weather-card-weather').innerHTML = data.weather[0].description;
+          document.getElementById('weather-card-temperature').innerHTML = Math.floor(data.main.temp) + tempUnit;
+          // document.getElementById('weather-card-weather').innerHTML = data.weather[0].description;
+          // document.getElementByClass('wi').className = data.weather[0].icon;
 
         });
       };
@@ -29,6 +38,10 @@ class Main extends React.Component {
       function error(err) {
         document.getElementById('local-weather').innerHTML('Cannot get weather for your location. Try again later.');
       };
+
+      $('#toggleTempUnit').on('click', function() {
+        tempUnit = tempUnit === '\u00B0C' ? '\u00B0F' : '\u00B0C'; 
+      });
 
       navigator.geolocation.getCurrentPosition(success, error);
 
@@ -46,9 +59,12 @@ class Main extends React.Component {
 
         <input id="input-field" type="text" placeholder="Enter city here"/>
         <div id="weather-card">
+          <div id="weather-card-weather"><i className="wi wi-owm-day-803"></i></div>
           <div id="weather-card-temperature"></div>
-          <div id="weather-card-weather"></div>
         </div>
+
+        <button id="toggleTempUnit"></button>
+
       </div>
     )
   }
